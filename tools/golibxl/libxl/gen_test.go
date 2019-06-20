5c83ca3ba21482c9c5ec155bacaae9fe808dcf02
Nick Rosbrook <rosbrookn@ainfosec.com>
2019-06-20 13:07:47 -0400

    Add test target in Makefile and add LDFLAGS to libxl.yml
    Signed-off-by: Nick Rosbrook <rosbrookn@ainfosec.com>

---
+++ tools/golibxl/libxl/gen_test.go
@@ -0,0 +1,11 @@
+package libxl
+
+import (
+	"testing"
+)
+
+// TestCodeGeneration tests the generated code by trying to access
+// some well-known constant.
+func TestCodeGeneration(t *testing.T) {
+	_ = MacFmt
+}
